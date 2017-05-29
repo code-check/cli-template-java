ea69dd25235620e1a99fa2904d6ff1c69a4fc9ff
TsubasaK111 <Tsubasa.K111@gmail.com>
2017-05-29 15:25:24 +0900

    feat: update output format

--- src/main/java/codecheck/example/App.java
+++ src/main/java/codecheck/example/App.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 
 public class App {
   public static void run(CommandLine cmd) {
-    Arrays.asList(cmd.getArgs()).forEach(v -> System.out.println(v));
+    Arrays.asList(cmd.getArgs()).forEach(arg, i ->
+      {
+        string result = String.format("argv[%i]: %v", i, arg);
+        System.out.println(result);
+      }
+    );
   }
 }
