1f63b8d38d32129e3db114cb9d4248fbe6499965
TsubasaK111 <Tsubasa.K111@gmail.com>
2017-05-29 17:07:32 +0900

    refactor: modify output format

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,7 +3,8 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			System.out.println(args[i]);
+			string output = String.format("argv[%i]: %a", i, args[i]);
+			System.out.println(output);
 		}
 	}
 }
