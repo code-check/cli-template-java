3259d64966f9c0c4c10b785fc3ee5f6fecb38384
TsubasaK111 <Tsubasa.K111@gmail.com>
2017-05-29 18:18:17 +0900

    fix: correct var declaration

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,7 +3,7 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			string output = String.format("argv[%i]: %a", i, args[i]);
+			String output = String.format("argv[%i]: %a", i, args[i]);
 			System.out.println(output);
 		}
 	}
